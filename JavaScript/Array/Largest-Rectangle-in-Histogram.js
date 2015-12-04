8c3d5b7e79af558dd48e325068a593aeef52717b
lirilsu <god@liril.net>
2015-12-04 11:55:40 +0800

    Add Largest Rectangle in Histogram

--- JavaScript/Array/Largest-Rectangle-in-Histogram.js
+++ JavaScript/Array/Largest-Rectangle-in-Histogram.js
@@ -3,15 +3,24 @@
  * @return {number}
  */
 var largestRectangleArea = function(height) {
-    height.push(0);
-    var arr = Array();
     var area = 0;
-    for (var i = 0; i < height.length; i++) {
-        if (arr.length == 0 || height[i] < arr.pop()) {
-
+    var arr = Array();
+    height.push(0);
+    var i = 0;
+    while (i < height.length) {
+        if (!arr.length || height[arr[arr.length - 1]] < height[i]) {
+            arr.push(i++);
+        } else {
+            var bar = arr.pop();
+            area = Math.max(area, height[bar] * (arr.length ? i - arr[arr.length - 1] - 1 : i));
         }
     }
     return area;
 };
 
-console.log(largestRectangleArea([2,1,5,6,2,3]));
+console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]));
+console.log(largestRectangleArea([2, 1, 2]));
+console.log(largestRectangleArea([1, 2, 2]));
+console.log(largestRectangleArea([4,2,0,3,2,5]));
+console.log(largestRectangleArea([3, 2, 5]));
+console.log(largestRectangleArea([3,6,5,7,4,8,1,0]));
